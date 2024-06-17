3dd821c8d4fa7b62033358eaf02ee9340885586e
Tahara Kazuki <>
2024-06-17 11:12:56 +0900

    update

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { createContext, PropsWithChildren, useContext } from 'react';
+
+type CardContextType = {
+  post: Post;
+};
+
+const CardContext = createContext<CardContextType | undefined>(undefined);
+
+const useCardContext = () => {
+  const context = useContext(CardContext);
+  if (!context) {
+    throw new Error('useCardContext must be used within a Card Component');
+  }
+  return context;
+};
+
 type Post = {
   id: number;
   title: string;
@@ -8,28 +26,42 @@ type Post = {
   };
 };
 
-type CardProps = {
+type CardProps = PropsWithChildren<{
   post: Post;
-};
+}>;
 
-const Card = ({ post }: CardProps) => {
+const Card = ({ post, children }: CardProps) => {
   return (
-    <div className="max-w-sm rounded-2xl shadow-2xl bg-white p-6">
-      <div className="px-6 py-4 space-y-3">
-        <div className="font-bold text-xl mb-2">{post.title}</div>
-        <p className="text-gray-700 text-base">{post.content}</p>
-        <p className="text-gray-700 text-sm text-center">{post.user.name}</p>
+    <CardContext.Provider value={{ post }}>
+      <div className="max-w-sm rounded-2xl shadow-2xl bg-white p-6">
+        <div className="px-6 py-4 space-y-3">{children}</div>
+
+        <div className="flex flex-row px-6 pt-4 pb-2 justify-between gap-3">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2">
+            Button
+          </button>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-1/2">
+            Cancel
+          </button>
+        </div>
       </div>
-      <div className="flex flex-row px-6 pt-4 pb-2 justify-between gap-3">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-1/2">
-          Button
-        </button>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-1/2">
-          Cancel
-        </button>
-      </div>
-    </div>
+    </CardContext.Provider>
   );
 };
 
 export default Card;
+
+Card.Title = function CardTitle() {
+  const { post } = useCardContext();
+  return <div className="font-bold text-xl mb-2">{post.title}</div>;
+};
+
+Card.Content = function CardContent() {
+  const { post } = useCardContext();
+  return <p className="text-gray-700 text-base">{post.content}</p>;
+};
+
+Card.UserName = function CardUserName() {
+  const { post } = useCardContext();
+  return <p className="text-gray-700 text-sm text-center">{post.user.name}</p>;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { title } from 'process';
+'use client';
 import Card from './components/Card';
 
 const dummyPostData = {
@@ -14,7 +14,11 @@ const dummyPostData = {
 export default function Home() {
   return (
     <main className="flex h-screen items-center justify-center bg-sky-200">
-      <Card post={dummyPostData} />
+      <Card post={dummyPostData}>
+        <Card.Title />
+        <Card.Content />
+        <Card.UserName />
+      </Card>
     </main>
   );
 }
